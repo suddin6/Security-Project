7701f723bfd57f9a3056d26b3801900047a02bfa
Sumaya Uddin <suddin6@oakland.edu>
2026-07-19 00:49:01 -0400

    Finished half of main screen

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
+'''
+Sumaya Uddin and Emma Hochron
+Group J - Cipher Machine [Project Implementation]
+Professor Solmaz Salehian
+CSI 3480 - Security and Privacy in Computing
+Due: August 13, 2026
+Description: An encryption-decryption tool that utilizes a GUI interface and allows for secure messaging!
+'''
+
 from tkinter import *
 from tkinter import messagebox
 import base64
 import os
 
+# Main GUI Screen for Cipher Machine Tool
 def screen():
-    # Main GUI Screen for Cipher Machine Tool
     screen = Tk()
     screen.geometry("400x400")
     
@@ -13,6 +22,17 @@ def screen():
     screen.iconphoto(False, favicon)
 
     screen.title("Cipher Machine")
+    
+    # Textbox for user input
+    Label(text="Enter Text:", fg="black", font=("calibri", 13)).place(x=20, y=20)
+    first_text = Text(font=("calibri", 13), bg="white", relief=GROOVE, wrap=WORD, bd=0)
+    first_text.place(x=20, y=50, width=355, height=100)
+
+    # Textbox for password input
+    Label(text="Enter Passcode:", fg="black", font=("calibri", 13)).place(x=20, y=160)
+    passcode = StringVar()
+    Entry(textvariable=passcode, width=19, bd=0, font=("calibri", 13), show="*").place(x=20, y=190)
+
     screen.mainloop()
 screen()
 
